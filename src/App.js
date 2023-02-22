368fa727196065967cd558da52f25d1aaabd3cd9
Diogo Henrique <diogomike138@gmail.com>
2023-02-22 11:07:41 -0300

    Feat: add função de favoritar produto e salvar no navegador do usuário.

--- src/App.js
+++ src/App.js
@@ -2,26 +2,33 @@ import React from 'react';
 import Produto from './Produto';
 
 function App() {
-  const [dados, setDados] = React.useState(null);
+  const [produto, setProduto] = React.useState(null);
   const [carregando, setCarregando] = React.useState(null);
 
-  async function handleClick(event) {
-    let produto = event.target.innerText
-    setCarregando(true);
-    const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
-    const json = await response.json();
-    
-    setDados(json);
-    setCarregando(false);
+  /* Carregando produto do navegador do usuário */
+  React.useEffect(() => {
+    const produtoLocal = window.localStorage.getItem('produto');
+    if(produtoLocal !== null) setProduto(produtoLocal)
+  }, [])
+  
+  /* Salvando produto no navegador do usuário */
+  React.useEffect(() => {
+    if(produto !== null) window.localStorage.setItem('produto', produto);
+  }, [produto])
+
+
+  /* Função para mudar o estado de produto */
+  function handleClick(event) {
+    setProduto(event.target.innerText); 
   }
 
   return (
     <div className="App">
+      <h1>Preferência: {produto}</h1>
       <button style={{ marginRight: '.5rem' }} onClick={handleClick}>Notebook</button>
       <button style={{ marginRight: '.5rem' }} onClick={handleClick}>Smartphone</button>
       <button style={{ marginRight: '.5rem' }} onClick={handleClick}>Tablet</button>
-      {carregando && <p>Carregando...</p>}
-      {!carregando && dados && <Produto dados={dados} />}
+      <Produto produto={produto} />
     </div>
   );
 }
--- src/Produto.js
+++ src/Produto.js
@@ -1,9 +1,22 @@
-const Produto = ({ dados }) => {
+import React from "react";
+
+const Produto = ({ produto }) => {
+    const [dados, setDados] = React.useState(null);
+
+    /* Carregando dados do produto da API*/
+    React.useEffect(()=> {
+        if(produto !== null) {
+            fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+            .then(response => response.json())
+            .then(json => setDados(json))
+        }   
+    }, [produto])
+
+    if(dados === null) return null;
     return (
         <div>
             <h1>{dados.nome}</h1>
             <p>R$ {dados.preco}</p>
-            <img src={dados.fotos[0].src} alt={dados.fotos[0].titulo} />
         </div>
     )
 }
